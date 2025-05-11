38fdef68aaab481213e524e63ad696f1026e2232
Miguel Ceriani <miguel.ceriani@gmail.com>
2025-05-11 15:15:19 +0200

    Add sudo for docker usage

--- build-lodview-img.sh
+++ build-lodview-img.sh
@@ -4,6 +4,6 @@ if [ -d "LodView" ]; then rm -Rf LodView; fi
 git clone --branch fix-war-plugin --single-branch https://github.com/miguel76/LodView
 cd LodView
 mvn compile war:war
-docker build -t lodview .
+sudo docker build -t lodview .
 cd ..
 rm -rf LodView
