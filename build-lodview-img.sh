a10aa27146cfd07b3cd2e37531b00ca2789d063f
Miguel Ceriani <miguel.ceriani@gmail.com>
2025-05-16 09:46:21 +0200

    Move lodview configuration to docker image

--- build-lodview-img.sh
+++ build-lodview-img.sh
@@ -2,6 +2,7 @@
 
 if [ -d "LodView" ]; then rm -Rf LodView; fi
 git clone --branch fix-war-plugin --single-branch https://github.com/miguel76/LodView
+cp ./lodview-conf.ttl ./LodView/src/main/webapp/WEB-INF/conf.ttl
 cd LodView
 mvn compile war:war
 sudo docker build -t lodview .
